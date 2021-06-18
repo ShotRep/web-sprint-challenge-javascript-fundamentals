6a0c2ba58e86c22cbd94dcf7af89e4a3849a3aa2
ShotRep <78008209+ShotRep@users.noreply.github.com>
2021-06-18 13:38:40 -0500

    incremental-starting topic 3 step 3 done

--- index.js
+++ index.js
@@ -216,6 +216,19 @@ function CuboidMaker(attributes) {
   this.height = attributes.height;
 }
 
+CuboidMaker.prototype.volume = function () {
+  return this.length * this.width * this.height;
+};
+
+CuboidMaker.prototype.surfaceArea = function () {
+  return (
+    2 *
+    (this.length * this.width +
+      this.length * this.height +
+      this.width * this.height)
+  );
+};
+
 /* 🐴🐴🐴 Step 2: Volume Method 🐴🐴🐴
   Create a method called volume using CuboidMaker's prototype that returns the volume of a given cuboid's length, width, and height
   Formula for cuboid volume: length * width * height   */
