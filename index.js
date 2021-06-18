3ad7b6f66aa9f41cc2b55649ffd73e1662e22373
ShotRep <78008209+ShotRep@users.noreply.github.com>
2021-06-18 10:25:29 -0500

    incremental-completed task 2 request 3

--- index.js
+++ index.js
@@ -138,9 +138,13 @@ lowerCaseNames(zooAnimals);
   Using lowPopulationAnimals use .filter() to create a new array of objects which contains only the animals with a population of less than 5.
   */
 
-function lowPopulationAnimals(/*Your Code Here*/) {
-  /*Your Code Here*/
+function lowPopulationAnimals(data) {
+  const lowPopulationArray = data.filter(function (item) {
+    return item.population < 5;
+  });
+  return lowPopulationArray;
 }
+lowPopulationAnimals(zooAnimals);
 
 /* 🦁🦁🦁 Request 4: .reduce() 🦁🦁🦁
   The zoo needs to know their total animal population across the United States. 
